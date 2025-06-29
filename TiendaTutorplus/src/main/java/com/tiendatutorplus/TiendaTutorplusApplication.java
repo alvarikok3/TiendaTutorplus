a957d62a62bae7ba1bd07786d126f523ebf489ea
alvarikok3 <107597713+alvarikok3@users.noreply.github.com>
2025-06-29 15:41:39 -0600

    Primer commit del proyecto TiendaTutorplus Signed-off-by:alvarikok3 <107597713+alvarikok3@users.noreply.github.com> Signed-off-by:alvarikok3 <107597713+alvarikok3@users.noreply.github.com>

---
+++ TiendaTutorplus/src/main/java/com/tiendatutorplus/TiendaTutorplusApplication.java
@@ -0,0 +1,11 @@
+package com.tiendatutorplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTutorplusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaTutorplusApplication.class, args);
+    }
+}
